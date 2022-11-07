0b41e6f6afaaa077f2f78c9c219a55a7e08d6f7a
Elisa Roca <roca_elisa2000@yahoo.com>
2022-11-07 19:33:21 +0200

    Add google font PT Sans, add api call in UseEffect() hook on home page

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,12 +1,35 @@
-import React from "react";
+import { useState, useEffect } from "react";
 import Form from "../components/Form/Form";
 import styles from "./Home.module.css";
 import milionnaireLogo from "../assets/logo-489x489.png";
+import { getCategories } from "../services/api";
 
 const Home = () => {
+  const [categories, setCategories] = useState(["All"]);
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const res = await getCategories();
+        if (res.status === 200) {
+          setCategories((prev) => {
+            if (prev.length === 1) {
+              return [...prev, ...Object.keys(res.data)];
+            } else {
+              return prev;
+            }
+          });
+        }
+      } catch (err) {
+        console.log("Something went wrong.", err.message);
+      }
+    };
+    getData();
+  }, []);
+
   return (
     <main className="d-flex justify-content-center align-items-center">
-      <Form />
+      <Form data={categories} />
       <div className={`d-flex align-items-center ${styles["main-image"]}`}>
         <img src={milionnaireLogo} alt="Who Wants to Be a Millionaire logo" />
       </div>
--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -4,19 +4,29 @@ import FormCheck from "./FormCheck";
 import Button from "../UI/Button";
 import styles from "./Form.module.css";
 
-function CheckExample() {
+const Form = (props) => {
+  const formCheckList = props.data.map((item) => (
+    <FormCheck
+      key={item.toLowerCase().replaceAll(" ", "-")}
+      id={item.toLowerCase().replaceAll(" ", "-")}
+    >
+      {item}
+    </FormCheck>
+  ));
+
   return (
-    <BSForm className={`d-flex justify-content-center flex-column text-white ${styles.form}`}>
-      <BSForm.Label className={`border border-white mb-1 ${styles["form-label"]}`}>
+    <BSForm
+      className={`d-flex justify-content-center flex-column text-white ${styles.form}`}
+    >
+      <BSForm.Label
+        className={`border border-white mb-1 ${styles["form-label"]}`}
+      >
         {UI_TEXT.selectCategoryMessage}
       </BSForm.Label>
-      <FormCheck id="1">Option 1</FormCheck>
-      <FormCheck id="2">Option 2</FormCheck>
-      <FormCheck id="3">Option 3</FormCheck>
-      <FormCheck id="4">Option 4</FormCheck>
+      {formCheckList}
       <Button className="mt-1">Submit</Button>
     </BSForm>
   );
-}
+};
 
-export default CheckExample;
+export default Form;

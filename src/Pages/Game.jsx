0097e9e8cc7a9847a4ac8387bfdbad81d879fabe
Elisa Roca <roca_elisa2000@yahoo.com>
2022-11-08 17:16:19 +0200

    Add api call in Game page useEffect()

--- src/Pages/Game.jsx
+++ src/Pages/Game.jsx
@@ -1,15 +1,48 @@
+import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import { getQuestions } from "../services/api";
+import Form from "../components/Form/Form";
 import styles from "./Game.module.css";
 
 const Game = () => {
+  const [questions, setQuestions] = useState();
   const { category } = useParams();
+
+  useEffect(() => {
+    const getData = async () => {
+      const res = await getQuestions(category);
+      try {
+        if (res.status === 200) {
+          setQuestions((prev) => {
+            if (!prev) {
+              return [...res.data];
+            } else {
+              return prev;
+            }
+          });
+        }
+      } catch (err) {
+        console.log("Something went wrong.", err.message);
+      }
+    };
+    getData();
+  }, []);
+
+  console.log(questions);
 
   return (
     <main
       className={`d-flex justify-content-center align-items-center ${styles["main-game"]}`}
     >
-      <div>{category}</div>
-      <div></div>
+      {questions && (
+        <Form
+          data={[...questions[0].incorrectAnswers, questions[0].correctAnswer]}
+          label={questions[0].question}
+          type={"questions"}
+          onGetSelectedCategory={null}
+          onSubmit={null}
+        />
+      )}
     </main>
   );
 };

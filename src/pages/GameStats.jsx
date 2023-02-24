5a4f90cfed062574b0dd40e46d3b26f4d0e7b969
Elisa Roca <roca_elisa2000@yahoo.com>
2023-02-24 10:45:16 +0200

    Change capitalization on folder name

---
+++ src/pages/GameStats.jsx
@@ -0,0 +1,88 @@
+import { useEffect } from "react";
+import { UserAuth } from "../authContext/AuthContext";
+import { getMostCommon } from "../services/utils";
+import { UI_TEXT } from "../services/constants";
+import Spinner from "../components/UI/Spinner";
+import styles from "./GameStats.module.css";
+
+const GameStats = () => {
+  const { user, getUserDoc, userData } = UserAuth();
+
+  useEffect(() => {
+    localStorage.clear();
+    getUserDoc(user);
+  }, [getUserDoc, user]);
+
+  return (
+    <main
+      className={`d-flex justify-content-center align-items-center ${styles["main-game-stats"]}`}
+    >
+      <ul className="d-flex flex-column justify-content-center align-items-center list-unstyled p-4">
+        {Object.keys(userData).length > 0 ? (
+          <>
+            <li>
+              {UI_TEXT.gameStats.gamesStarted}
+              <span>{userData.gamesStarted}</span>
+            </li>
+            <br />
+            <li>
+              {UI_TEXT.gameStats.gamesWon}
+              <span>{userData.gamesWon}</span>
+            </li>
+            <li>
+              {UI_TEXT.gameStats.gamesEnded}
+              <span>{userData.gamesEnded}</span>
+            </li>
+            <li>
+              {UI_TEXT.gameStats.gamesLost}
+              <span>{userData.gamesLost}</span>
+            </li>
+            <li>
+              {UI_TEXT.gameStats.gamesAbandoned}
+              <span>
+                {userData.gamesStarted -
+                  userData.gamesWon -
+                  userData.gamesEnded -
+                  userData.gamesLost}
+              </span>
+            </li>
+            <br />
+            <li>
+              {UI_TEXT.gameStats.preferredCategory}
+              <span>
+                {userData.gamesStarted
+                  ? getMostCommon(userData.categories)
+                  : "-"}
+              </span>
+            </li>
+            <br />
+            <li>
+              {UI_TEXT.gameStats.totalQuestionsAnswered}
+              <span>{userData.answersTime.numberOfAnswers}</span>
+            </li>
+            <li>
+              {UI_TEXT.gameStats.averageTimeToAnswer}
+              <span>
+                {userData.answersTime.numberOfAnswers > 0
+                  ? `${(
+                      userData.answersTime.totalAnswerTime /
+                      userData.answersTime.numberOfAnswers
+                    ).toFixed(2)}s`
+                  : "-"}
+              </span>
+            </li>
+            <br />
+            <li>
+              {UI_TEXT.gameStats.lifelinesUsed}
+              <span>{userData.lifelinesUsed}</span>{" "}
+            </li>
+          </>
+        ) : (
+          <Spinner />
+        )}
+      </ul>
+    </main>
+  );
+};
+
+export default GameStats;
---
+++ src/pages/SignIn.jsx
@@ -0,0 +1,63 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { UI_TEXT } from "../services/constants";
+import AuthForm from "../components/AuthForm/AuthForm";
+import { UserAuth } from "../authContext/AuthContext";
+
+const SignIn = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const { signIn } = UserAuth();
+  const navigate = useNavigate();
+
+  const formTitle = "Sign In";
+  const formType = "signin";
+  const additionalFormText = UI_TEXT.authForm.suggestSignUp;
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    setError("");
+
+    try {
+      await signIn(email, password);
+      navigate("/");
+    } catch (err) {
+      if (err.code === "auth/wrong-password") {
+        setError(UI_TEXT.authFormErrors.incorrectPassword);
+      }
+      if (err.code === "auth/user-not-found") {
+        setError(UI_TEXT.authFormErrors.userNotFound);
+      } if (err.code === "auth/too-many-requests") {
+        setError(UI_TEXT.authFormErrors.tooManyRequests);
+      }
+      else {
+        setError(err.code);
+      }
+    }
+  };
+
+  const onChangeEmail = (value) => {
+    setEmail(value);
+  };
+
+  const onChangePassword = (value) => {
+    setPassword(value);
+  };
+
+  return (
+    <AuthForm
+      formTitle={formTitle}
+      formType={formType}
+      additionalFormText={additionalFormText}
+      onSubmit={handleSubmit}
+      onChangeEmail={onChangeEmail}
+      onChangePassword={onChangePassword}
+      emailValue={email}
+      passwordValue={password}
+      error={error}
+    />
+  );
+};
+
+export default SignIn;
